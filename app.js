bfeca93e7f1d7a76cc2a924c892476c2d8af6cac
Janjira012 <s5830213012@phuket.psu.ac.th>
2018-12-11 22:57:29 +0700

    add code

---
+++ app.js
@@ -0,0 +1,76 @@
+var express = require('express');
+var app = express();
+var fs = require("fs");
+const body = require('body-parser')
+
+
+app.use(body.json())
+
+app.get('/getUser', function (req, res) {
+
+    fs.readFile(__dirname + "/" + "db.json", 'UTF-8', function (err, data) {
+
+        res.end(data);
+    });
+});
+
+app.get('/getUser/:id', function (req, res) {
+    fs.readFile(__dirname + "/" + "db.json", 'UTF-8', function (err, data) {
+        var users = JSON.parse(data);
+        for (i = 0; i < users["staffcoc"].length; i++) {
+            //var user = users["staffcoc"][i][req.params.id];
+            var data = users["staffcoc"][i];
+            if (data.id == req.params.id) {
+                res.end(JSON.stringify(data));
+            }
+        }
+        //res.end(JSON.stringify(user));
+    });
+});
+// var user ={
+//     "staffcoc" : [
+//         {
+//                 "id": 1,
+//                 "staffName": "Sinchai",
+//                 "staffLastName": "Kamolphiwong",
+//                 "position": "Assoc.Prof.Dr."            
+//         }
+//     ]
+// }
+
+app.post('/addUser', function (req, res) {
+    fs.readFile(__dirname + "/" + "db.json", 'UTF-8', function (err, data) {
+        data = JSON.parse(data);
+        var sizeuser = data["staffcoc"].length;
+        var userdata = {
+            "id": sizeuser + 1,
+            "staffName": req.body.staffName,
+            "staffLastName": req.body.staffLastName,
+            "position": req.body.position
+        }
+        data["staffcoc"].push(userdata)
+        fs.writeFile(__dirname + "/" + "db.json", JSON.stringify(data), (err, data) => {
+            console.log(data)
+        })
+    });
+});
+
+app.delete('/deleteUser/:index', function (req, res) {
+    fs.readFile(__dirname + "/" + "db.json", 'UTF-8', function (err, data) {
+        var users = JSON.parse(data);
+        for (i = 0; i < users["staffcoc"].length; i++) {
+            //var user = users["staffcoc"][i][req.params.id];
+            var data = users["staffcoc"][i];
+            if (data.id == req.params.index) {
+                res.end(JSON.stringify(data));
+            }
+        }
+        //res.end(JSON.stringify(user));
+    });
+});
+
+var server = app.listen(8081, function () {
+    var host = server.address().address
+    var port = server.address().port
+    console.log("Application Run At http://%s:%s", host, port);
+})
